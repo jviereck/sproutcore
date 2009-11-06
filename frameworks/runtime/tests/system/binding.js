ce08c51baedfb0d541a7cda36d747adacbae147c
Charles Jolley <charles@sproutit.com>
2009-11-06 12:01:25 -0800

    Add import statements to unit tests.

--- frameworks/runtime/tests/system/binding.js
+++ frameworks/runtime/tests/system/binding.js
@@ -3,6 +3,12 @@
 // ========================================================================
 /*globals module test ok isObj equals expects */
 
+"import core_test:package";
+"import core";
+"import system/binding";
+"import system/object";
+"import system/run_loop";
+
 var fromObject, toObject, binding, Bon1, bon2 ; // global variables
 
 module("basic object binding", {
@@ -377,3 +383,5 @@ test("toObject.value should be second value if first is falsy", function() {
   SC.Binding.flushPendingChanges();
   equals(toObject.get('value'), 'second value');
 });
+
+plan.run();
